192541c49ec13642c95fdc3e546569c4a68c85d8
Hilau67 <hugo.huilier@viacesi.fr>
2018-06-01 14:49:46 +0200

    Initial commit

--- FileSaver/MainWindow.xaml.cs
+++ FileSaver/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace FileSaver
 {
@@ -20,9 +9,104 @@ namespace FileSaver
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		/// <summary>
+		/// 
+		/// </summary>
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			if(IsDirectory())
+			{
+				string text = CopyFilesFromDirectory(from.Text, to.Text) ? "File or directory copied" : "Error !" ;
+				MessageBox.Show(text);
+			}
+			else
+			{
+				try
+				{
+					File.Copy(from.Text, to.Text);
+				}
+				catch (DirectoryNotFoundException)
+				{
+					MessageBox.Show("File to copy or destination directory doesn't exist");
+				}
+				catch(FileNotFoundException)
+				{
+					MessageBox.Show("File to copy doesn't exist");
+				}
+				catch (Exception ex)
+				{
+					throw ex;
+				}				
+			}
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
+		bool IsDirectory()
+		{
+			try
+			{
+				FileAttributes attr = File.GetAttributes(from.Text);
+
+				return ((attr & FileAttributes.Directory) == FileAttributes.Directory);
+			}
+			catch (Exception ex)
+			{
+				throw ex;
+			}			
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="sourcePath"></param>
+		/// <param name="destinationPath"></param>
+		/// <returns></returns>
+		bool CopyFilesFromDirectory(string sourcePath, string destinationPath)
+		{
+			try
+			{
+				if (Directory.Exists(sourcePath))
+				{
+					if (!Directory.Exists(destinationPath))
+					{
+						Directory.CreateDirectory(destinationPath);
+					}
+
+					foreach (string file in Directory.GetFiles(sourcePath))
+					{
+						var fileInfo = new FileInfo(file);
+						fileInfo.CopyTo(destinationPath + @"\" + fileInfo.Name, true);
+					}
+
+					foreach (string directory in Directory.GetDirectories(sourcePath))
+					{
+						var directoryInfo = new DirectoryInfo(directory);
+						CopyFilesFromDirectory(directory, destinationPath + @"\" + directoryInfo.Name);
+					}
+
+					return true;
+				}
+
+				return false;
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+			
 		}
 	}
 }
